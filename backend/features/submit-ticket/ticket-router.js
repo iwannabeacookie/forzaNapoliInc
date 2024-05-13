df4535242d4ebab7d16045a1c8c0ace85f5d72d7
iwannabeacookie <buevad@gmail.com>
2024-05-13 12:35:12 +0200

    Contact support api made more RESTful

--- backend/features/submit-ticket/ticket-router.js
+++ backend/features/submit-ticket/ticket-router.js
@@ -3,7 +3,7 @@ import Ticket from "./models/ticketModel.js";
 
 const router = express.Router();
 
-router.post("/api/submit-ticket", (req, res) => {
+router.post("/api/ticket", (req, res) => {
   new Ticket({
     date: Date.now(),
     phone: req.body.phone,
@@ -13,11 +13,9 @@ router.post("/api/submit-ticket", (req, res) => {
     .save()
     .then(() => {
       res.status(200).end();
-      // console.log("Ticket successfully written");
     })
     .catch((err) => {
       res.status(500).send(err);
-      // console.log("Ticket write operation failed");
     });
 });
 
--- frontend/src/components/contact-support/scripts/post.js
+++ frontend/src/components/contact-support/scripts/post.js
@@ -4,7 +4,7 @@ export function postTicket(body) {
   console.info("[API] POST Request, body:", body);
   const API_URL = "http://localhost:3000";
   return axios
-    .post(`${API_URL}/api/submit-ticket`, body)
+    .post(`${API_URL}/api/ticket`, body)
     .then((response) => {
       return response.data;
     })
