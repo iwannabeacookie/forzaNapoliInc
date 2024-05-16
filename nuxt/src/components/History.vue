073ab58a0cec9229910f91b2cc60753c06217c7a
Alessandro Guida <129603940+Drive4Code@users.noreply.github.com>
2024-05-16 14:45:55 +0200

    Fixed Missing Import

--- nuxt/src/components/History.vue
+++ nuxt/src/components/History.vue
@@ -27,8 +27,7 @@ export default {
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-    </a>
+    <a href="https://vitejs.dev" target="_blank"> </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
