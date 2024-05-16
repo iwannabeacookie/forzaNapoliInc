9eb0836bb5655488ba1e9881ea0a0b415bc9a0db
Alessandro Guida <129603940+Drive4Code@users.noreply.github.com>
2024-05-16 15:30:54 +0200

    Fixed Cart display

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,13 +1,13 @@
 <template>
-  <ul>
-    <li>
-      <NuxtLink to="/cart">Home</NuxtLink>
-    </li>
-    <li>
-      <NuxtLink to="/history">History</NuxtLink>
-    </li>
-    <li>
-      <NuxtLink to="/support">Support</NuxtLink>
-    </li>
-  </ul>
+    <ul>
+        <li>
+            <NuxtLink to="/cart">Cart</NuxtLink>
+        </li>
+        <li>
+            <NuxtLink to="/history">History</NuxtLink>
+        </li>
+        <li>
+            <NuxtLink to="/support">Support</NuxtLink>
+        </li>
+    </ul>
 </template>
