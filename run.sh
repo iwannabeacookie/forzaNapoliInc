33d57578c5cd7d324b99500ead90eb4825aeae7f
iwannabeacookie <buevad@gmail.com>
2024-06-24 11:57:39 +0200

    test(run): testing single process run

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# cd ./frontend
+# NODE_DEBUG=cluster,net,http,fs,tls,module,timers NITRO_PORT=10000 node ./.output/server/index.mjs &
+# echo "Running script"
+#
+# cd ../backend
+# NODE_DEBUG=cluster,net,http,fs,tls,module,timers node ./server.js &
+#
 cd ./frontend
-NODE_DEBUG=cluster,net,http,fs,tls,module,timers NITRO_PORT=10000 node ./.output/server/index.mjs &
-echo "Running script"
-
-cd ../backend
-NODE_DEBUG=cluster,net,http,fs,tls,module,timers node ./server.js &
+NITRO_PORT=10000 node ./.output/server/index.mjs
