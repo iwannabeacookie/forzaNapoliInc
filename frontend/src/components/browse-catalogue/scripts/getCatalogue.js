30dfdeb9b437a9fc3207dfd2d1c3d42ffb0f1d02
iwannabeacookie <buevad@gmail.com>
2024-06-25 00:58:51 +0200

    test: runtimeConfig test withing fetch function call

--- frontend/src/components/browse-catalogue/scripts/getCatalogue.js
+++ frontend/src/components/browse-catalogue/scripts/getCatalogue.js
@@ -3,6 +3,7 @@ import { useRuntimeConfig } from "nuxt/app";
 const API_URL = useRuntimeConfig().apiUrl;
 
 export default function getCatalogue() {
+    const API_URL = useRuntimeConfig().apiUrl;
   console.info("[API] Requesting catalogue...");
     console.info("[API] URL:", `${API_URL}/api/catalogue`);
   return axios
