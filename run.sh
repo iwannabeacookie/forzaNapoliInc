24e0921f2dfccc44acc183808428a89cb1fc8cb0
iwannabeacookie <buevad@gmail.com>
2024-06-24 12:01:58 +0200

    test(single run): cleanup test(run): second process not running in bg

--- run.sh
+++ run.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# cd ./frontend
-# NODE_DEBUG=cluster,net,http,fs,tls,module,timers NITRO_PORT=10000 node ./.output/server/index.mjs &
-# echo "Running script"
-#
-# cd ../backend
-# NODE_DEBUG=cluster,net,http,fs,tls,module,timers node ./server.js &
-#
 cd ./frontend
-NITRO_PORT=10000 node ./.output/server/index.mjs
+NODE_DEBUG=cluster,net,http,fs,tls,module,timers NITRO_PORT=10000 node ./.output/server/index.mjs &
+echo "Running script"
+
+cd ../backend
+NODE_DEBUG=cluster,net,http,fs,tls,module,timers node ./server.js
