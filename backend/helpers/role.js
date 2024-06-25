a1bf117901d005d444d585497b17037b3988fe75
Alessandro Guida <129603940+Drive4Code@users.noreply.github.com>
2024-06-25 14:31:15 +0200

    refactor: Whitespace

--- backend/helpers/role.js
+++ backend/helpers/role.js
@@ -1,69 +1,69 @@
 import usercollection from "../models/userModel.js";
 
 export function isCustomer(req, res, next) {
-    req.sessionStore.get(req.body.sessionid._value, async (error, session) => {
-        if (session) {
-            if (session.passport) {
-                const user = await usercollection.findOne({
-                    _id: session.passport.user.id,
-                });
-                if (user.role === "Customer") {
-                    return next();
-                }
-            }
+  req.sessionStore.get(req.body.sessionid._value, async (error, session) => {
+    if (session) {
+      if (session.passport) {
+        const user = await usercollection.findOne({
+          _id: session.passport.user.id,
+        });
+        if (user.role === "Customer") {
+          return next();
         }
-    });
+      }
+    }
+  });
 
-    res.status(500);
+  res.status(500);
 }
 
 export function isAdmin(req, res, next) {
-    req.sessionStore.get(req.body.sessionid._value, async (error, session) => {
-        if (session) {
-            if (session.passport) {
-                const user = await usercollection.findOne({
-                    _id: session.passport.user.id,
-                });
-                if (user.role === "Admin") {
-                    return next();
-                }
-            }
+  req.sessionStore.get(req.body.sessionid._value, async (error, session) => {
+    if (session) {
+      if (session.passport) {
+        const user = await usercollection.findOne({
+          _id: session.passport.user.id,
+        });
+        if (user.role === "Admin") {
+          return next();
         }
-    });
+      }
+    }
+  });
 
-    res.status(500);
+  res.status(500);
 }
 
 export function isModerator(req, res, next) {
-    req.sessionStore.get(req.body.sessionid._value, async (error, session) => {
-        if (session) {
-            if (session.passport) {
-                const user = await usercollection.findOne({
-                    _id: session.passport.user.id,
-                });
-                if (user.role === "Moderator") {
-                    return next();
-                }
-            }
+  req.sessionStore.get(req.body.sessionid._value, async (error, session) => {
+    if (session) {
+      if (session.passport) {
+        const user = await usercollection.findOne({
+          _id: session.passport.user.id,
+        });
+        if (user.role === "Moderator") {
+          return next();
         }
-    });
+      }
+    }
+  });
 
-    res.status(500);
+  res.status(500);
 }
 
 export function isSupport(req, res, next) {
-    req.sessionStore.get(req.body.sessionid._value, async (error, session) => {
-        if (session) {
-            if (session.passport) {
-                const user = await usercollection.findOne({
-                    _id: session.passport.user.id,
-                });
-                if (user.role === "Support") {
-                    return next();
-                }
-            }
+  req.sessionStore.get(req.body.sessionid._value, async (error, session) => {
+    if (session) {
+      if (session.passport) {
+        const user = await usercollection.findOne({
+          _id: session.passport.user.id,
+        });
+        if (user.role === "Support") {
+          return next();
         }
-    });
+      }
+    }
+  });
 
-    res.status(500);
+  res.status(500);
 }
